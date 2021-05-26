0a7c37326ebab766e12836d42dc126200bdd4264
dambert <dambert05@gmail.com>
2021-05-26 12:11:32 -0500

    listo

---
+++ Sesion16/JS/misScripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("a").click(function(evento){
+        alert("Has pulsado el enlace. Ahora serás enviado a la página de la USAT");
+    });
+});
+
+//ocultar/mostrar bloque
+$("#c1b").click(function(){
+    $("#cont1").toggle(1500);
+}, function(){
+    $("#cont1").toggle(1500);
+});
